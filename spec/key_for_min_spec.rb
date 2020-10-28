c58ebd20365f9fad94fae6e93616661abc26254d
Adam Walker <adamw207@yahoo.com>
2020-10-28 16:17:16 +0000

    Done.

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -49,11 +49,11 @@ describe "smallest hash value" do
   end
 
   it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
+    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).not_to eq(:adam)
   end
 
   it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
+    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).not_to eq(:blake)
   end
 
   it "returns nil for an empty hash" do
